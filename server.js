1076fdee641b0269c5213938efe899986bcc2ce6
cyvu <christofferdahlberg@gmail.com>
2023-03-15 21:47:45 +0100

    cors

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const bodyParser = require('body-parser')
-const express = require('express')
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
 // const MongoClient = require('mongodb').MongoClient
-const Database = require('./classes/database')
+const Database = require("./classes/database");
 // const db = require('./config/db')   // TODO keep?
 
-const app = express()
-const port = 8000
+const app = express();
+const port = 8000;
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 /*
 MongoClient.connect(db.url, (err, database) => {
@@ -16,13 +17,19 @@ MongoClient.connect(db.url, (err, database) => {
 })
 */
 
-const db = new Database()
+const db = new Database();
 
 if (db.isConnected()) {
-  console.log("Database connected")
-  require('./app/routes')(app, db)
-} else
-  console.error("db not connected")
+  console.log("Database connected");
+  require("./app/routes")(app, db);
+} else console.error("db not connected");
 
+const corsOptions = {
+  origin: "http://127.0.0.1:5500",
+};
 
-app.listen(port, () => { console.log('We are live on ' + port)})
+app.use(cors(corsOptions));
+
+app.listen(port, () => {
+  console.log("We are live on " + port);
+});
